b49d75af15077a254fbcc344e745c494f788b814
= <=>
2024-05-14 23:38:16 +0530

    corrected app.py and test.py

--- test_app.py
+++ test_app.py
@@ -1,16 +1,7 @@
-import pytest
 from app import app
 
-class TestApp(pytest.TestCase):
 
-    def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
-
-    def test_homepage(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), 'Hello World')
-
-if __name__ == '__main__':
-    pytest.main()
+def test_hello():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello world!!!'
